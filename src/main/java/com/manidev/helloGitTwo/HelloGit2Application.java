b7d17e8bf300a0347566dd3e27bd71330aa56baa
Mukarram Mukhtar <mukarram@hotmail.com>
2020-03-28 13:22:18 -0500

    First commit in git 2

---
+++ src/main/java/com/manidev/helloGitTwo/HelloGit2Application.java
@@ -0,0 +1,14 @@
+package com.manidev.helloGitTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGit2Application.class, args);
+		System.out.println("Hello world git 2!");
+	}
+
+}
